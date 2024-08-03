5ef3f92cb240a02d92fbcbe40464668a9e56e87f
Grzegorz Kowalski <g.kowalski@pumox.com>
2024-08-03 16:19:09 +0200

    Add empty MVC project with Identity (Individual Accounts).

---
+++ HotDesk.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HotDesk.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
